e4bfde871dff9a3f89e9864cfa9c64d36cc6583f
dagon0721 <b5959946c@gmail.com>
2024-11-13 16:44:47 +0900

    Update 1113

---
+++ ui-frame/src/app/react-icon/page.jsx
@@ -0,0 +1,12 @@
+import { DiApple } from "react-icons/di";
+import { DiChrome } from "react-icons/di";
+
+export default function ReactIcon() {
+  return (
+    <>
+      <h1>React icon</h1>
+      <DiApple size={300} color="red" />
+      <DiChrome size={300} color="red" />
+    </>
+  );
+}
